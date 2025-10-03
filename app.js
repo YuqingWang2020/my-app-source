4d82c22115f4bf5c383cdac38dde1a472ea62025
yuqingwang2020 <yuqingwang2020@gmail.com>
2025-10-03 13:50:06 +0200

    Test CI/CD with Docker Hub secrets

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from my automated CI/CD pipeline - Version 2!\n');
+  res.end('Hello World from my automated CI/CD pipeline - Version 3!\n');
 });
 
 server.listen(port, () => {

6012d4bf3d1a7aef3e11d07a191462cc67ea96db
yuqingwang2020 <yuqingwang2020@gmail.com>
2025-10-03 22:05:53 +0200

    V5

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from my automated CI/CD pipeline - Version 4!\n');
+  res.end('Hello World from my automated CI/CD pipeline - Version 5!\n');
 });
 
 server.listen(port, () => {
